3d17f30f5b0f0a9e6094318ac35002ad8c51ebc0
Aloshi <allofquist@yahoo.com>
2013-01-08 11:19:38 -0600

    Changed OpenGL min texture filter to Linear for GuiImage.

--- src/components/GuiImage.cpp
+++ src/components/GuiImage.cpp
@@ -152,7 +152,7 @@ void GuiImage::loadImage(std::string path)
 
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, imageRGBA);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
