acba3bd258beb731710da1e26346769d23ca5fe9
Bim Overbohm <bim.overbohm@googlemail.com>
2013-05-14 21:54:47 +0200

    Remove .cpp #includes
    It is cleaner to not do that and just include the proper file in the
    build files if needed.

--- src/Renderer_init.cpp
+++ src/Renderer_init.cpp
@@ -1,12 +1,7 @@
+#include "Renderer.h"
 #include "platform.h"
-
-#ifdef _RPI_
-	#include "Renderer_init_rpi.cpp"
-#endif
-
-#ifdef _DESKTOP_
-	#include "Renderer_init_sdlgl.cpp"
-#endif
+#include GLHEADER
+#include "Font.h"
 
 namespace Renderer
 {
