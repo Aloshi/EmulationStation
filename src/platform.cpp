4b4c891b303c939928f5dbc873a4f3a33726cf5c
Bim Overbohm <bim.overbohm@googlemail.com>
2013-05-16 21:29:41 +0200

    Find proper home path
    Should work on more systems now.

--- src/platform.cpp
+++ src/platform.cpp
@@ -1,12 +1,31 @@
 #include "platform.h"
 #include <stdlib.h>
 
+
 std::string getHomePath()
 {
-	//this gives you something like "/home/YOUR_USERNAME" on Linux and "C:\Users\YOUR_USERNAME\" on Windows
-	const char* home = getenv("HOME");
-	if(home == NULL)
-		return "";
-	else
-		return home;
+	std::string homePath;
+
+	//this should give you something like "/home/YOUR_USERNAME" on Linux and "C:\Users\YOUR_USERNAME\" on Windows
+	const char * envHome = getenv("HOME");
+	if(envHome != nullptr) {
+		homePath = envHome;
+	}
+#ifdef WIN32
+	//but does not seem to work for Windwos XP or Vista, so try something else
+	if (homePath.empty()) {
+		const char * envDir = getenv("HOMEDRIVE");
+		const char * envPath = getenv("HOMEPATH");
+		if (envDir != nullptr && envPath != nullptr) {
+			homePath = envDir;
+			homePath += envPath;
+		}
+	}
+#else
+	if (homePath.empty()) {
+		homePath = "~";
+	}
+#endif
+
+	return homePath;
 }
