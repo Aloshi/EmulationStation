33c28d2bc492bcab7b9f7b11ddeffea824a232d3
Matthieu <matthieu@mknet.fr>
2014-07-21 13:42:26 +0200

    added dependency to boost regex, finished mamedbscraper

--- es-app/src/scrapers/MamedbScraper.cpp
+++ es-app/src/scrapers/MamedbScraper.cpp
@@ -1,79 +1,95 @@
 #include "MamedbScraper.h"
 #include "Log.h"
-#include "pugixml/pugixml.hpp"
+
+#include <boost/regex.hpp>
 
 void mamedb_generate_scraper_requests(const ScraperSearchParams& params, std::queue< std::unique_ptr<ScraperRequest> >& requests,
 	std::vector<ScraperSearchResult>& results)
 {
 	std::string path = "www.mamedb.com/game/";
-
-	std::string cleanName = params.nameOverride;
-	if(cleanName.empty())
-		cleanName = params.game->getName();
-
+        
+	std::string cleanName = params.game->getPath().filename().replace_extension("").c_str();
 	path += HttpReq::urlEncode(cleanName);
 	
-	/* TODO REMOVE TRACE */
-	std::stringstream ss;
-	ss << "mamedb - request.\n\t" << path;
-	std::string err = ss.str();
-	LOG(LogError) << err;
-	/*UNTIL HERE*/
 	requests.push(std::unique_ptr<ScraperRequest>(new MamedbRequest(results, path)));
+
 }
 
+boost::regex infolineregex("^.*?<h1>Game Details</h1>(.*?)</table>.*$");
+        boost::regex titleregex("^.*?<b>Name:&nbsp</b>(?<title>.*?)<br/>.*?<b>Year:&nbsp</b> \
+<a href='/year/.*?'>(?<date>.*?)</a><br/>\
+<b>Manufacturer:&nbsp</b> <a href='/manufacturer/.*?'>(?<developer>.*?)</a><br/>\
+<b>Filename:&nbsp;</b>(?<filename>.*?)<br/><b>.*$");
+        boost::regex cloneregex("^.*?&nbsp;\\(clone of: <a href='.*?'>(?<clone>.*?)</a>\\)&nbsp;<br/>.*$");
+        boost::regex cleantitleregex("^(?<title>.*?)&nbsp.*$");
+        boost::regex scoreregex("^.*?<b>Score:&nbsp;</b>(?<rating>.*?) \\(.*? votes\\)<br/>.*$");
+        boost::regex genreregex("^.*?<b>Category:&nbsp;</b><a href='.*?'>(?<genre>.*?)</a><br/>.*$");
+        boost::regex playersregex("^.*?<b>Players:&nbsp;</b>(?<players>.*?)<br/>.*$");
+        
 void MamedbRequest::process(const std::unique_ptr<HttpReq>& req, std::vector<ScraperSearchResult>& results)
 {
 	assert(req->status() == HttpReq::REQ_SUCCESS);
+  
+        
 
-	pugi::xml_document doc;
-	pugi::xml_parse_result parseResult = doc.load(req->getContent().c_str());
-	/* TODO REMOVE TRACE */
-	std::stringstream ss;
-	ss << "MamedbRequest - parsing XML.\n\t" << req->getContent().c_str();
-	std::string err = ss.str();
-	LOG(LogError) << err;
-	/*UNTIL HERE*/
-	if(!parseResult)
-	{
-		std::stringstream ss;
-		ss << "MamedbRequest - error parsing XML.\n\t" << parseResult.description();
-		std::string err = ss.str();
-		setError(err);
-		LOG(LogError) << err;
-		return;
-	}
-
-
-
-	pugi::xml_node data = doc.child("OpenSearchDescription").child("games");
-
-	pugi::xml_node game = data.child("game");
-	while(game && results.size() < MAX_SCRAPER_RESULTS)
-	{
-		ScraperSearchResult result;
-
-		result.mdl.set("name", game.child("title").text().get());
-		result.mdl.set("desc", game.child("description").text().get());
-
-		//Archive.search does not return ratings
-
-		result.mdl.set("developer", game.child("developer").text().get());
-
-		std::string genre = game.child("genre").text().get();
-		size_t search = genre.find_last_of(" &gt; ");
-		genre = genre.substr(search == std::string::npos ? 0 : search, std::string::npos);
-		result.mdl.set("genre", genre);
-
-		pugi::xml_node image = game.child("box_front");
-		pugi::xml_node thumbnail = game.child("box_front_small");
-
-		if(image)
-			result.imageUrl = image.text().get();
-		if(thumbnail)
-			result.thumbnailUrl = thumbnail.text().get();
-
-		results.push_back(result);
-		game = game.next_sibling("game");
-	}
+        boost::smatch infolinematches;
+        if (boost::regex_match(req->getContent(), infolinematches, infolineregex)){
+            
+            boost::smatch linematches;
+            std::string line(infolinematches[1]);
+            if (boost::regex_match(line, linematches, titleregex)){
+                ScraperSearchResult result;
+                
+                // TITLE
+                result.mdl.set("name", std::string(linematches["title"]));
+                boost::smatch tmatches;
+                if (boost::regex_match(std::string(linematches["title"]), tmatches, cleantitleregex)){
+                    result.mdl.set("name", std::string(tmatches["title"]));
+                }
+                // DATE
+                result.mdl.set("releasedate", std::string(linematches["date"]));
+                
+                // DEVELOPPER
+                if(std::string(linematches["developer"]).compare(std::string("<unknown></unknown>")) != 0){
+                    result.mdl.set("developer", std::string(linematches["developer"]));
+                }
+                //GENRE
+                boost::smatch genrematches;
+                if (boost::regex_match(line, genrematches, genreregex)){
+                    result.mdl.set("genre", std::string(genrematches["genre"]));
+                }
+                
+                //RATING
+                boost::smatch scorematches;
+                if (boost::regex_match(req->getContent(), scorematches, scoreregex)){
+                    float score = 0;
+                    std::stringstream ( std::string(scorematches["rating"]) ) >> score;
+                    score = score / 10.0f;
+                    result.mdl.set("rating", std::to_string(score));
+                }
+               
+                //PLAYERS
+                boost::smatch playersmatches;
+                if (boost::regex_match(line, playersmatches, playersregex)){
+                    result.mdl.set("players", std::string(playersmatches["players"]));
+                }
+                
+                // IMAGES
+                boost::smatch clonematches;
+                std::stringstream ss;
+                if(boost::regex_match(line, clonematches, cloneregex)){
+                    ss << "http://www.mamedb.com/snap/"<<std::string(clonematches["clone"]) <<".png";
+                }else {
+                    ss << "http://www.mamedb.com/snap/"<< std::string(linematches["filename"]) <<".png"; 
+                }
+                result.imageUrl = ss.str();
+                result.thumbnailUrl = ss.str();
+                
+                results.push_back(result);
+            }
+        }else {
+            LOG(LogInfo) << req->getContent().c_str();
+            LOG(LogInfo) << titleregex.str() << "\nNot found";
+            
+        }
 }
