ce00f5f8ce9f7744efe940389437a9480bf05907
aslater <adam.j.slater@gmail.com>
2015-08-30 17:53:37 -0500

    Make system sort type persistent

--- es-core/src/components/OptionListComponent.h
+++ es-core/src/components/OptionListComponent.h
@@ -251,7 +251,6 @@ public:
 		onSelectedChanged();
 	}
 
-private:
 	unsigned int getSelectedId()
 	{
 		assert(mMultiSelect == false);
@@ -265,6 +264,7 @@ private:
 		return 0;
 	}
 
+private:
 	void open()
 	{
 		mWindow->pushGui(new OptionListPopup(mWindow, this, mName));
