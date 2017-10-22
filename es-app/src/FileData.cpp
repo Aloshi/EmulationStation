cd2f2ee42b911711db6bd6e0276bea494a389e88
Tomas Jakobsson <tompsson@hotmail.com>
2017-10-23 01:04:17 +0200

    Use local game art ( image, marquee and video ) from romfolder/images if the gamelist didn't specify a path

--- es-app/src/FileData.cpp
+++ es-app/src/FileData.cpp
@@ -43,12 +43,32 @@ std::string FileData::getCleanName() const
 	return removeParenthesis(this->getDisplayName());
 }
 
-const std::string& FileData::getThumbnailPath() const
+const std::string FileData::getThumbnailPath() const
 {
-	if(!metadata.get("thumbnail").empty())
-		return metadata.get("thumbnail");
-	else
-		return metadata.get("image");
+	std::string thumbnail = metadata.get("thumbnail");
+
+	// no thumbnail, try image
+	if(thumbnail.empty())
+	{
+		thumbnail = metadata.get("image");
+
+		// no image, try to use local image
+		if(thumbnail.empty())
+		{
+			const char* extList[2] = { ".png", ".jpg" };
+			for(int i = 0; i < 2; i++)
+			{
+				if(thumbnail.empty())
+				{
+					std::string path = mEnvData->mStartPath + "/images/" + getDisplayName() + "-image" + extList[i];
+					if(boost::filesystem::exists(path))
+						thumbnail = path;
+				}
+			}
+		}
+	}
+
+	return thumbnail;
 }
 
 const std::string& FileData::getName()
@@ -76,14 +96,63 @@ const std::vector<FileData*>& FileData::getChildrenListToDisplay() {
 	}
 }
 
-const std::string& FileData::getVideoPath() const
+const std::string FileData::getVideoPath() const
 {
-	return metadata.get("video");
+	std::string video = metadata.get("video");
+
+	// no video, try to use local video
+	if(video.empty())
+	{
+		std::string path = mEnvData->mStartPath + "/images/" + getDisplayName() + "-video.mp4";
+		if(boost::filesystem::exists(path))
+			video = path;
+	}
+
+	return video;
 }
 
-const std::string& FileData::getMarqueePath() const
+const std::string FileData::getMarqueePath() const
 {
-	return metadata.get("marquee");
+	std::string marquee = metadata.get("marquee");
+
+	// no marquee, try to use local marquee
+	if(marquee.empty())
+	{
+		const char* extList[2] = { ".png", ".jpg" };
+		for(int i = 0; i < 2; i++)
+		{
+			if(marquee.empty())
+			{
+				std::string path = mEnvData->mStartPath + "/images/" + getDisplayName() + "-marquee" + extList[i];
+				if(boost::filesystem::exists(path))
+					marquee = path;
+			}
+		}
+	}
+
+	return marquee;
+}
+
+const std::string FileData::getImagePath() const
+{
+	std::string image = metadata.get("image");
+
+	// no image, try to use local image
+	if(image.empty())
+	{
+		const char* extList[2] = { ".png", ".jpg" };
+		for(int i = 0; i < 2; i++)
+		{
+			if(image.empty())
+			{
+				std::string path = mEnvData->mStartPath + "/images/" + getDisplayName() + "-image" + extList[i];
+				if(boost::filesystem::exists(path))
+					image = path;
+			}
+		}
+	}
+
+	return image;
 }
 
 std::vector<FileData*> FileData::getFilesRecursive(unsigned int typeMask, bool displayedOnly) const
