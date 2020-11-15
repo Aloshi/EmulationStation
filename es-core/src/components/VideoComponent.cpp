398f6096cbbc985214df1f897a7c4d8328ff72e7
Tomas Jakobsson <tompsson@hotmail.com>
2020-11-15 21:59:30 +0100

    Disable video playback if video component is hidden

--- es-core/src/components/VideoComponent.cpp
+++ es-core/src/components/VideoComponent.cpp
@@ -295,8 +295,8 @@ void VideoComponent::update(int deltaTime)
 void VideoComponent::manageState()
 {
 	// We will only show if the component is on display and the screensaver
-	// is not active
-	bool show = mShowing && !mScreensaverActive && !mDisable;
+	// is not active and the component is visible
+	bool show = mShowing && !mScreensaverActive && !mDisable && mVisible;
 
 	// See if we're already playing
 	if (mIsPlaying)
